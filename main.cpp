6970ae0bf65c345817dda8f0db89c30e7ccbb3bc
Tommaso Cari Gallingani <tommycarigallingani@gmail.com>
2019-11-06 09:33:24 +0000

    aggiunte le opzioni e main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
+void menu();
+void inserisciContatto(char nome, char numero);
+void eliminaContatto();
+void stampaAgenda();
 
 int main(){
-  cout << "Benvenuto nella rubrica";
+  string scelta;
+  menu();
+  cout << "Scegli un'opzione: ";
+  cin >> scelta;
 }
 
+void menu(){
+  cout << "Benvenuto nella rubrica \n";
+  cout << "A) Inserisci contatto" << endl;
+  cout << "B) Elimina contatto" << endl;
+  cout << "C) Lista contatto" << endl;
+  cout << "D) Esci" << endl;
+}
+
+  
+
 /*
+inserisciContatto
+eliminaContatto
+stampaAgenda
+
+MAIN
+void scriviMenu
+
 string addressbook[20] = {
 
 }
